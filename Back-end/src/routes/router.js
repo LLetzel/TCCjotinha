4160fdae502a2a57f5a214fb82566ded9bbab96f
Gabizete <gabrielledelimaq@gmail.com>
2025-05-07 11:29:14 -0300

    email contato pronto

--- Back-end/src/routes/router.js
+++ Back-end/src/routes/router.js
@@ -13,10 +13,11 @@ const carsController = require('../controllers/carsController.js')
 const nodemailer = require("nodemailer");
 require("dotenv").config({ path: "../.env" });
 const multer = require('multer');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../../uploads')); 
+        cb(null, path.join(__dirname, '../../uploads'));
     },
     filename: function (req, file, cb) {
         const ext = path.extname(file.originalname);
@@ -80,21 +81,31 @@ router.post("/contato", async (req, res) => {
             }
         });
 
+        console.log(__dirname)
+        const templatePath = path.join("./email", 'emailContato.html');
+        let htmlTemplate = fs.readFileSync(templatePath, 'utf8');
+
+        htmlTemplate = htmlTemplate
+            .replace('{{name}}', name)
+            .replace('{{email}}', email)
+            .replace('{{phone}}', phone)
+            .replace('{{subject}}', subject)
+            .replace('{{message}}', message);
+
 
         // Configuração do e-mail
         const mailOptions = {
             from: `"${name}" <${email}>`,
             to: "gabrielledelimaq@gmail.com",
             subject: `Nova mensagem de contato: ${subject}`,
-            html: `
-                <h2>Nova mensagem de contato</h2>
-                <p><strong>Nome:</strong> ${name}</p>
-                <p><strong>E-mail:</strong> ${email}</p>
-                <p><strong>Telefone:</strong> ${phone}</p>
-                <p><strong>Assunto:</strong> ${subject}</p>
-                <p><strong>Mensagem:</strong></p>
-                <p>${message}</p>
-            `
+            html: htmlTemplate,
+            attachments: [
+                {
+                  filename: 'logo.png',
+                  path: path.join(__dirname, '../../../Front-end/img/logo.png'),
+                  cid: 'logo'
+                }
+              ]
         };
 
         // Enviar o e-mail
@@ -112,6 +123,7 @@ router.post("/contato", async (req, res) => {
 
 
 router.use(express.static(path.join(__dirname, "../../../Front-end")));
+
 
 router.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../Front-end/src/login/login.html"));
@@ -166,39 +178,6 @@ router.get("/sobrenos", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../Front-end/src/sobrenos/sobrenos.html"));
 });
 
-//------------------------------PESSOAL senha--------------------------------//
 
-// router.put("/perfil/alterar-senha/:id", async (req, res) => {
-//     const { senha } = req.body; // Pegando o telefone do corpo da requisição
-//     // const userId = req.session.userId; 
-//     const userId = req.params.id;// Pegando o ID do usuário autenticado na sessão
-
-//     // Verificar se o telefone foi enviado
-//     if (!senha) {
-//         return res.status(400).json({ mensagem: "A senha não pode estar vazio." });
-//     }
-
-//     // Verificar se o usuário está autenticado
-//     if (!userId) {
-//         return res.status(401).json({ mensagem: "Usuário não autenticado." });
-//     }
-
-//     const sql = "UPDATE usuarios SET senha = ? WHERE id = ?";
-
-//     try {
-//         // Usando o promisePool para executar a consulta
-//         const [result] = await promisePool.query(sql, [senha, userId]);
-
-//         // Verificando se a atualização foi bem-sucedida
-//         if (result.affectedRows > 0) {
-//             res.json({ mensagem: "Senha atualizada com sucesso!" });
-//         } else {
-//             res.status(404).json({ mensagem: "Usuário não encontrado." });
-//         }
-//     } catch (err) {
-//         console.error("Erro ao atualizar senha:", err);
-//         res.status(500).json({ mensagem: "Erro ao atualizar senha." });
-//     }
-// });
 
 module.exports = router;
