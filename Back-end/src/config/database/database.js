66579eac817b00ee56fbf2949e5eb4a0244d4dad
LLetzel <lucasdiasletzel@gmail.com>
2025-05-08 17:23:10 -0300

    arrumando a variaveis de ambiente

--- Back-end/src/config/database/database.js
+++ Back-end/src/config/database/database.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/../../../.env' });
 
 module.exports = {
   development: {
-    username: process.env.DB_NAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
-    logging: process.env.DB_LOGGING === 'true',
+    logging: process.env.DB_LOGGING === 'true'
   },
 };
