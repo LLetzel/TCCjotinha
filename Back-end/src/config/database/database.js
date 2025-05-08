aaaafe3e127b524ea9d9a5fd6647ea90dffdaac8
LLetzel <lucasdiasletzel@gmail.com>
2025-05-08 16:38:55 -0300

    arrumando rotas do servidor

--- Back-end/src/config/database/database.js
+++ Back-end/src/config/database/database.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || 'mysql',
     logging: process.env.DB_LOGGING === 'true',
   },
 };
