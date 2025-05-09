49f1fa38d41e514e68829b559b54ada5242d8b1d
Gabizete <gabrielledelimaq@gmail.com>
2025-05-09 09:39:08 -0300

    agendamento com email pro cliente pronto

--- Back-end/src/controllers/agendamentoController.js
+++ Back-end/src/controllers/agendamentoController.js
@@ -7,6 +7,9 @@ const { any } = require('../utils/multer.js');
 const User = require('../models/user.js');
 const { response } = require('express');
 const Interesse = require('../models/destaques.js');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const fs = require('fs');
 
 // Função para verificar se um registro existe em uma tabela
 async function checkIfExists(column, table, info) {
@@ -28,7 +31,7 @@ exports.getAgendamentos = async (req, res) => {
 };
 
 // Função para criar agendamentos (POST)
-exports.postAgendamentos = async (req, res) => {    
+exports.postAgendamentos = async (req, res, next) => {    
     try {
         const { id_usuario, id_carro, interesse, data, hora, observacoes } = req.body;
         
@@ -50,11 +53,64 @@ exports.postAgendamentos = async (req, res) => {
             return res.status(400).json({ message: 'Dados inválidos' });
         }
 
-        // Criando o agendamento
         const create = await Agendamento.create({ id_usuario, id_carro, interesse, data, hora, observacoes });
-        return res.status(201).json({ message: 'Agendamento criado com sucesso!', data: create });
+        res.locals.create = create;
+        next();
     } catch (error) {
         return res.status(500).json({ message: 'Erro ao criar agendamento', error });
     }
 };
+
+exports.emailAgendamento = async (req, res) => {
+    try {
+            const { email, data, hora, interesse, observacoes} = req.body;
+            // Validação dos campos obrigatórios
+            if (!email || !data || !hora || !interesse || !observacoes ) {
+                return res.status(400).json({ message: "Todos os campos são obrigatórios" });
+            }
+    
+            // Configuração do transporte de e-mail (usando variáveis de ambiente)
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: process.env.EMAIL_USER, // Definido no .env
+                    pass: process.env.EMAIL_PASS // App Password do Gmail
+                }
+            });
+    
+            
+            const templatePath = path.join("./email", 'emailAgendamento.html');
+            let htmlTemplate = fs.readFileSync(templatePath, 'utf8');
+    
+            htmlTemplate = htmlTemplate
+                .replace('{{data}}', data)
+                .replace('{{hora}}', hora)
+                .replace('{{interesse}}', interesse)
+                .replace('{{observacoes}}', observacoes);
+    
+    
+            // Configuração do e-mail
+            const mailOptions = {
+                from: "Jotinha veículos",
+                to: email,
+                subject: "Informações do seu agendamento",
+                html: htmlTemplate,
+                attachments: [
+                    {
+                      filename: 'logo.png',
+                      path: path.join(__dirname, '../../../Front-end/img/logo.png'),
+                      cid: 'logo'
+                    }
+                  ]
+            };
+    
+            // Enviar o e-mail
+            await transporter.sendMail(mailOptions);
+    
+            return res.status(200).json({ message: 'Agendamento criado com sucesso, confira seu email!', data: res.locals.create });
+        } catch (error) {
+            console.error("Erro ao enviar e-mail:", error);
+            return res.status(500).json({ message: "Não foi possível enviar o e-mail, tente novamente" });
+        }
+}
 
